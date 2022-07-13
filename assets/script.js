3c280810a80a045967a578872722c4359a328fa6
Alpha Bravo <bautista.albert@gmail.com>
2022-07-13 01:49:43 -0500

    Update script.js for ready function and index.html for local storage check

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,55 @@
-var today = moment().format("dddd, MMMM Do")
+var today = moment().format("dddd, MMMM Do, YYYY")
 $("#currentDay").text(today)
 
+$(document).ready(function () {
+    // on click save time and text 
+    $(".saveBtn").on("click", function () {
+        // grab time and text
+        var time = $(this).parent().attr("id")
+        var text = $(this).siblings(".description").val()
+
+        // save into local storage array
+        localStorage.setItem(time, text)
+        $("#localStorage").text("Task saved! \u2713")
+    })
+   
+    function timeChecker() {
+        // get current number of hours
+        var currentTime = moment().hour()
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var timeBlock = parseInt($(this).attr("id").split("hour")[1])
+
+            // Check the time and current state
+            if (timeBlock < currentTime) {
+                $(this).removeClass("future")
+                $(this).removeClass("present")
+                $(this).addClass("past")
+            }
+            else if (timeBlock === currentTime) {
+                $(this).removeClass("past")
+                $(this).removeClass("future")
+                $(this).addClass("present")
+            }
+            else {
+                $(this).removeClass("present")
+                $(this).removeClass("past")
+                $(this).addClass("future")
+            }
+        })
+    }
+
+    // Get item from local storage if any
+    $("#nineAm .description").val(localStorage.getItem("nineAm"));
+    $("#tenAm .description").val(localStorage.getItem("tenAm"));
+    $("#elevenAm .description").val(localStorage.getItem("elevenAm"));
+    $("#twelvePm .description").val(localStorage.getItem("twelvePm"));
+    $("#onePm .description").val(localStorage.getItem("onePm"));
+    $("#twoPm .description").val(localStorage.getItem("twoPm"));
+    $("#threePm .description").val(localStorage.getItem("threePm"));
+    $("#fourPm .description").val(localStorage.getItem("fourPm"));
+    $("#fivePm .description").val(localStorage.getItem("fivePm"));
+
+    timeChecker();
+})
